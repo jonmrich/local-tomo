02d50b0a627512ae639cae4934ef7786880e322b
jonmrich <jonmrich@gmail.com>
2017-10-05 20:48:13 -0400

    Load jquery

--- js/testing.js
+++ js/testing.js
@@ -1,5 +1,19 @@
-$(document).ready(function() {
+
+
+jQueryCode = function(){
+   $(document).ready(function() {
 	$('.12345').append("hello");
 	$('.67890').append("goodbye");
 
 });
+}
+
+if(window.jQuery)  jQueryCode();
+else{   
+    var script = document.createElement('script'); 
+    document.head.appendChild(script);  
+    script.type = 'text/javascript';
+    script.src = "//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js";
+
+    script.onload = jQueryCode;
+}
