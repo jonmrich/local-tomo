8659dcf52e164abd1e40dddc01c6a6e51314e7b0
jonmrich <jonmrich@gmail.com>
2017-10-06 08:46:07 -0400

    Add autocomplete

--- js/testing.js
+++ js/testing.js
@@ -1,3 +1,11 @@
+ 
+//TO DO
+
+//Pick place thing with here's your code
+//pick styles (load the right css)
+//code you've got? Need jquery or bootstrap?
+//run locally? we can package it up for download.
+
  function loadcss(url) {
      var head = document.getElementsByTagName('head')[0],
          link = document.createElement('link');
@@ -7,16 +15,15 @@
      head.appendChild(link);
      return link;
  }
- loadcss('https://immense-brook-56331.herokuapp.com/css/styles.css');
+ //loadcss('https://immense-brook-56331.herokuapp.com/css/styles.css');
  $(document).ready(function() {
      $('.tomo').each(function() {
          var tomoClass = $(this).prop("classList");
          var firstClass = tomoClass[1];
-var split = firstClass.split("-");
-
-var currId = split[0];
-var pickedStyle = split[1];
-console.log(pickedStyle)
+         var split = firstClass.split("-");
+         var currId = split[0];
+         var pickedStyle = split[1];
+         console.log(pickedStyle)
          $.ajax({
              url: 'https://immense-brook-56331.herokuapp.com/php/fetch.php',
              type: 'POST',
@@ -28,42 +35,27 @@ console.log(pickedStyle)
                  var parsed = $.parseJSON(data);
                  mainData = parsed.data;
                  $('.' + firstClass).empty();
-                 if (pickedStyle=="full"){
-           	console.log("full")
-
-                 var imgUrl = mainData.images[0].image_url;
-                 $('.' + firstClass).append('<div class="placeName"><h3>' + mainData.name + '</h3></div><div class="placeDescription">' + mainData.description + '</div><div class="placeImage"><img src="' + imgUrl + '"></div><div class="tomoAttribution">Powered by <a href="https://tomo.co">Tomo</a></div>');
-             }
-             else if (pickedStyle=="lite"){
-           	console.log("lite")
-
-             	 var imgUrl = mainData.images[0].image_url;
-                 $('.' + firstClass).append('<div class="placeName"><h3>' + mainData.name + '</h3></div><div class="placeImage"><img src="' + imgUrl + '"></div><div class="tomoAttribution">Powered by <a href="https://tomo.co">Tomo</a></div>');
-           }
-           else if(pickedStyle =="img"){
-           	console.log("img")
-           	if(mainData.images[0]){
-                 var imgUrl1 = mainData.images[0].image_url;
-
+                 if (pickedStyle == "full") {
+                     var imgUrl = mainData.images[0].image_url;
+                     $('.' + firstClass).append('<div class="placeName"><h3>' + mainData.name + '</h3></div><div class="placeDescription">' + mainData.description + '</div><div class="placeImage"><img src="' + imgUrl + '"></div><div class="tomoAttribution">Powered by <a href="https://tomo.co">Tomo</a></div>');
+                 } else if (pickedStyle == "lite") {
+                     $('.' + firstClass).append('<div class="placeName"><h3>' + mainData.name + '</h3></div><div class="placeDescription">' + mainData.description + '</div><div class="tomoAttribution">Powered by <a href="https://tomo.co">Tomo</a></div>');
+                 } else if (pickedStyle == "imgs") {
+                     if (mainData.images[0]) {
+                         var imgUrl1 = mainData.images[0].sizes.square.url;
+                     }
+                     if (mainData.images[1]) {
+                         var imgUrl2 = mainData.images[1].sizes.square.url;
+                     }
+                     if (mainData.images[2]) {
+                         var imgUrl3 = mainData.images[2].sizes.square.url;
+                     }
+                     $('.' + firstClass).append('<div class="placeName"><h3>' + mainData.name + '</h3></div><div class="placeImage"><img class="imgBuff" src="' + imgUrl1 + '"><img class="imgBuff" src="' + imgUrl2 + '"><img class="imgBuff" src="' + imgUrl3 + '"></div><div class="tomoAttribution">Powered by <a href="https://tomo.co">Tomo</a></div>');
                  }
-                 if(mainData.images[1]){
-                 var imgUrl2 = mainData.images[1].image_url;
-
-                 }
-                 if(mainData.images[2]){
-                 var imgUrl3 = mainData.images[2].image_url;
-                 }
-
-                 $('.' + firstClass).append('<div class="placeName"><h3>' + mainData.name + '</h3></div><div class="placeImage"><img class="imgBuff" src="' + imgUrl1 + '"><img class="imgBuff" src="' + imgUrl2 + '"><img class="imgBuff" src="' + imgUrl3 + '"></div><div class="tomoAttribution">Powered by <a href="https://tomo.co">Tomo</a></div>');
-                 $('.' + firstClass).append('<div class="placeName"><h3>' + mainData.name + '</h3></div><div class="placeImage"><img class="imgBuff" src="' + imgUrl1 + '"><img class="imgBuff" src="' + imgUrl2 + '"><img class="imgBuff" src="' + imgUrl3 + '"></div><div class="tomoAttribution">Powered by <a href="https://tomo.co">Tomo</a></div>');
-           
-           }
-             
              }
          });
      });
-
- });//ready
+ }); //ready
  /*jQueryCode = function(){
 
  }
---
+++ php/auto_complete.php
@@ -0,0 +1,23 @@
+<?php
+
+$name       = $_POST["query"];
+$latitude 	= (float)$_POST["latitude"];
+$longitude  = (float)$_POST["longitude"];
+$clean_name = addslashes($name);
+require_once '../functions.php';
+$callType = 'GET/POST';
+$endpoint = 'autocomplete';
+$returned = 'full';
+
+$mydata = array(
+    'input' => $name,
+    'radius'=> 100000,
+    'latitude'=>$latitude,
+    'longitude' =>$longitude
+);
+$data_json = json_encode($mydata);
+
+$curl = curl_init();
+require '../curl.php';
+
+
