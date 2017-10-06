75ff516872d1532d078c9210f92fd57fc0e431e0
jonmrich <jonmrich@gmail.com>
2017-10-06 08:50:53 -0400

    File locations

--- php/auto_complete.php
+++ php/auto_complete.php
@@ -5,7 +5,7 @@ $name       = $_POST["query"];
 $latitude 	= (float)$_POST["latitude"];
 $longitude  = (float)$_POST["longitude"];
 $clean_name = addslashes($name);
-require_once '../functions.php';
+require_once 'functions.php';
 $callType = 'GET/POST';
 $endpoint = 'autocomplete';
 $returned = 'full';
@@ -19,6 +19,6 @@ $mydata = array(
 $data_json = json_encode($mydata);
 
 $curl = curl_init();
-require '../curl.php';
+require 'curl.php';
 
 
